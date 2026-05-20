b9f6f26c92bf7ef3f6447d4799077e9613c3f1a8
megansummers316 <megan.summers16@outlook.com>
2026-05-20 13:41:02 -0400

    add colour page

--- MCCC/Controllers/HomeController.cs
+++ MCCC/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace MCCC.Controllers
             return View();
         }
 
+        public IActionResult Colours()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

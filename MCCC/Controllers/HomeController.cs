7d0d8b22a33f3fe945a9e37794a29a25fdcd619d
megansummers316 <megan.summers16@outlook.com>
2026-06-03 14:05:45 -0400

    send button working to send an email to my outlook account

--- MCCC/Controllers/HomeController.cs
+++ MCCC/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using System.Diagnostics;
 using MCCC.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 namespace MCCC.Controllers
 {
@@ -30,7 +34,32 @@ namespace MCCC.Controllers
 
         public IActionResult ContactMe()
         {
+
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Contact(string name, string address1, string address2, string postalCode, string email, List<string> itemDesc, List<string> colour, List<IFormFile> itemPhoto)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("megscozycrochetcrafts@gmail.com");
+            message.To.Add("megan.summers16@outlook.com");
+            message.Subject = "New Crochet Order";
+            string body = "";
+            body += $"Name: {name}\n";
+            body += $"Email: {email}\n";
+            body += $"Items:\n";
+            for (int i = 0; i < itemDesc.Count; i++)
+            {
+                body += $"\nItem: {i + 1}\n";
+                body += $"Description: {itemDesc[i]}\n";
+                body += $"Colour: {colour[i]}\n";
+            }
+            message.Body = body;
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new NetworkCredential("megscozycrochetcrafts@gmail.com", "iylyfgsdtkpppjsp");
+            client.EnableSsl = true;
+            await client.SendMailAsync(message);
+            return RedirectToAction("Index");
         }
 
         public IActionResult Colours()
